8a300cba3b488a086b7747a7fd373f5c8f21750d
aderemilekun <adelefatai@yahoo.com>
2023-11-22 15:54:52 +0100

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Aderemilekun Adele on 22/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
